b510950ea091f1104d1b707f6bd85104e623a518
Rizki Tirta Buwono <rizkipou01@gmail.com>
2021-04-17 06:45:38 +0700

    complete the to do application

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,79 @@
 <template>
-  
+  <div class="container" style="margin-top: 20px">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Simple Todo App</h5>
+        <div class="row">
+          <div class="col-10">
+            <input
+              v-model="todo"
+              type="text"
+              class="form-control"
+              @keyup.enter="add"
+            />
+          </div>
+          <div class="col-auto">
+            <button class="btn btn-success" @click="add">Add</button>
+          </div>
+        </div>
+        <list :todos="todos" @deleteTodo="deleteTodo" @doneTodo="doneTodo" />
+        <small>Total Todo : {{ totalTODO }}</small>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import List from "./components/List.vue";
 export default {
+  components: { List },
+  data() {
+    return {
+      todo: "",
+      todos: [],
+    };
+  },
+  mounted() {
+    this.todos = JSON.parse(localStorage.getItem('todos'));
+  },
+  computed: {
+    totalTODO() {
+      return this.todos.length;
+    },
+  },
+  methods: {
+    add() {
+      this.todos.unshift({
+        activity: this.todo,
+        isDone: false,
+      });
+      this.todo = "";
+      this.saveToLocalSTorage();
+    },
+    deleteTodo(todoIndex) {
+      this.todos = this.todos.filter((item, index) => {
+        if (index != todoIndex) {
+          return item;
+        }
+      });
+      this.saveToLocalSTorage();
+    },
+    doneTodo(todoIndex) {
+      this.todos = this.todos.filter((item, index) => {
+        if (index == todoIndex) {
+          item.isDone = true;
+        }
 
-}
+        return item;
+      });
+      this.saveToLocalSTorage();
+    },
+    saveToLocalSTorage() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="mt-2">
+    <ul class="list-group">
+      <li class="list-group-item" v-for="(todo, index) in todos" :key="index">
+        <div class="row">
+          <div class="col">
+            <span v-if="todo.isDone">
+              <del>{{ todo.activity }}</del>
+            </span>
+            <span v-else>{{todo.activity}}</span>
+          </div>
+          <div class="col-auto">
+            <div class="row gx-2">
+              <div class="col">
+                <button @click="done(index)" class="btn btn-info">Done</button>
+              </div>
+              <div class="col">
+                <button @click="deleteTODO(index)" class="btn btn-danger">
+                  X
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default: [],
+    },
+  },
+  methods: {
+    deleteTODO(index) {
+      this.$emit("deleteTodo", index);
+    },
+    done(index) {
+      this.$emit('doneTodo', index)
+    },
+  },
+};
+</script>
+
+<style>
+</style>
